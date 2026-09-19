ac25aed1f7e5a5e172b9bccc93099122aa58a62e
teelur <tylershea12@gmail.com>
2026-09-19 11:51:30 -0500

    setup build with yarn

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   plugins: [react()],
   build: {
+    emptyOutDir: false,
     lib: {
       entry: resolve(import.meta.dirname, 'src/index.ts'),
       fileName: 'index',
