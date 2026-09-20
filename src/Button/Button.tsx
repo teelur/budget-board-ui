e0ed24b45e0941cf4f42a48065a003cd95f064c5
teelur <tylershea12@gmail.com>
2026-09-20 14:17:16 -0500

    remove unstyled

--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -50,7 +50,13 @@ export interface ButtonProps
     >,
     Omit<
       UnstyledButtonProps,
-      "children" | "color" | "disabled" | "size" | "style" | "variant"
+      | "children"
+      | "color"
+      | "disabled"
+      | "size"
+      | "style"
+      | "unstyled"
+      | "variant"
     > {
   selected?: boolean;
   children?: ReactNode;
